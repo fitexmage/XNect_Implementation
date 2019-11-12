4ecdbc3404d3ba228e32d3c53a11da33c4edf19b
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-11 23:39:07 -0500

    Fix a bug about loading model.

--- model/model_provider.py
+++ model/model_provider.py
@@ -17,7 +17,7 @@ def parse_criterion(criterion):
 
 def create_model(opt):
     model = Stage_1_Model(num_joints=19, num_paf=17, only_2d=True)
-    if os.listdir(opt.saveDir) > 1:
+    if len(os.listdir(opt.saveDir)) > 1:
         model.load_state_dict(torch.load(os.path.join(opt.saveDir, 'model_{}.pth'.format(1))))
         print("Successfully load the model!")
     else:
