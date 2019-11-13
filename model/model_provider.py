e2f14517bd03e129b6b28debe42c1b8b72549b9d
Shengguang Bai <fitexmage@shengguangs-mbp.home>
2019-11-13 18:02:37 -0500

    Add a function to find the latest model.

--- model/model_provider.py
+++ model/model_provider.py
@@ -4,7 +4,7 @@ from .paf_model import PAFModel
 import torch.nn as nn
 import torch
 import os
-
+import re
 
 def parse_criterion(criterion):
     if criterion == 'l1':
@@ -15,10 +15,23 @@ def parse_criterion(criterion):
         raise ValueError('Criterion ' + criterion + ' not supported')
 
 
+def find_latest_file(dir):
+    max_num = 0
+    max_file = ""
+    for file in os.listdir(dir):
+        result = re.findall('[0-9]+', file)
+        if len(result) > 0:
+            if max_num < int(result[0]):
+                max_num = int(result[0])
+                max_file = file
+    return max_file
+
+
 def create_model(opt):
     model = Stage_1_Model(num_joints=19, num_paf=17, only_2d=True)
     if len(os.listdir(os.path.join(opt.saveDir, 'model'))) > 0:
-        model.load_state_dict(torch.load(os.path.join(opt.saveDir, 'model_{}.pth'.format(7))))
+        latest_file = find_latest_file(os.path.join(opt.saveDir, 'model'))
+        model.load_state_dict(torch.load(latest_file))
         print("Successfully load the model!")
     else:
         model.load_state_dict(torch.load('../save/SelecSLS60_statedict.pth'), strict=False)
